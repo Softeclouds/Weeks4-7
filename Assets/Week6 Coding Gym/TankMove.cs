83249a7f9ad77d8bbcb13b0e777e9aba3ccfbdd1
SofteClouds <kaimck05@gmail.com>
2025-02-13 14:16:22 -0500

    Made Tank move and the barrel aim with the mouse

---
+++ Assets/Week6 Coding Gym/TankMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMove : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;       // Create new Vector2 called pos that is equal to the obejcts position
+        if (Input.GetKey(KeyCode.D))            // if either D, A, W, or S are pressed, move character accordingly with speed
+        {
+            pos.x += speed * Time.deltaTime;    // Time.deltaTime is used to keep the movement consistent, instead of using the framerate, which could cause inconsistent movement
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;             // the objects position is set to the pos vector
+    }
+}
